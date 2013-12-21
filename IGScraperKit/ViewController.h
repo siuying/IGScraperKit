bc4a133b125ed2cd2c9e8c2a80390dce35cbba0a
Francis Chong <francis@ignition.hk>
2013-12-21 18:57:38 +0800

    Initial Commit

---
+++ IGScraperKit/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IGScraperKit
+//
+//  Created by Francis Chong on 12/21/13.
+//  Copyright (c) 2013 Francis Chong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
